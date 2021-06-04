96d48ae3c897c6c3a50660b678937ba8a354a143
Matt Shore <44679408+MatthewShoreWeb@users.noreply.github.com>
2021-06-04 15:16:43 +0100

    Update script.py

--- script.py
+++ script.py
@@ -6,18 +6,26 @@ currentMouseX, currentMouseY = pyautogui.position();
 print(currentMouseX, currentMouseY);
 
 def load_game():
-    pyautogui.click(888, 623) # Swap world
-    time.sleep(5)
-    pyautogui.click(693, 980) # Selects first non mobile map.
-    time.sleep(10)
-    pyautogui.click(1262, 623) # Manual village placement
-    time.sleep(2)
-    # pyautogui.click(x, y) # Whatever the coordinates of the county are.
+    pyautogui.doubleClick(311, 189) # load game from main menu, location might change depending on how many apps are installed
+    time.sleep(20) # wait for game to load
+    print('Game loaded')
+    pyautogui.click(1172, 596) # I am a new player!
+    time.sleep(1)
+    pyautogui.click(1048, 771) # Terms of service
+    time.sleep(10) # tries to load global realms.
+    pyautogui.click(909, 637) # swap worlds
+    time.sleep(5) # wait for load
+    # pyautogui.click(x, y) # depends what world needs loading
+    time.sleep(10) # wait for load
+    pyautogui.click(1266, 629) # manual
+    time.sleep(1)
+    # pyautogui.click(x, y) # county placement depends where to spawn
+    pyautogui.click(1008, 614) # place village
 
-    # We don't need to do the tutorial I think. We can just exit the game and clear the cache then repeat.
-    pyautogui.move(515, 12)
+    pyautogui.move(515, 12) # now we just exit 
     time.sleep(1)
     pyautogui.click(515, 12)    
+    clear_cache() # wipe cache and repeat
 
 def clear_cache():
     pyautogui.click(542, 196) # system apps button
@@ -26,12 +34,15 @@ def clear_cache():
     time.sleep(1)
     pyautogui.click(402, 330) # storage button
     time.sleep(1)
-    for x in range(8): # go down the list till we reach cache option
-        pyautogui.press('down')
-    pyautogui.press('enter') # press cache option
-    time.sleep(1) # wait for box to appear (should be instant)
+    pyautogui.click(307, 477) # apps
+    time.sleep(1)
+    pyautogui.click(330, 333) # shk
+    time.sleep(1) 
+    pyautogui.click(433, 524)
+    time.sleep(1)
     pyautogui.click(1407, 646) # press ok
     pyautogui.move(515, 13) 
     pyautogui.click(515, 13) # back to main menu
+    load_game()
 
-clear_cache()
+#clear_cache()
